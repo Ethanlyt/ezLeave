2b27b3d5219de5c4aab0a6537e2c22211b6a5f82
Ethanlyt <ethanleong86@gmail.com>
2022-05-26 02:17:04 +0800

    v1.5

---
+++ javascript/managerHome.js
@@ -0,0 +1,36 @@
+function dropdownFunction() {
+    document.getElementById("SortDropdown").classList.toggle("show");
+  }
+  
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdow.show
+        ns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
---
+++ javascript/timeclock.js
@@ -0,0 +1,37 @@
+
+const months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"];
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const day = document.getElementById('date-day');
+    const month = document.getElementById('date-mon');
+    const dow = document.getElementById('date-dow');
+    const hr = document.getElementById('clock-hr');
+    const min = document.getElementById('clock-min');
+    const sec = document.getElementById('clock-sec');
+    const ampm = document.getElementById('clock-ampm');
+    const colons = document.querySelectorAll('.clock-colon');
+
+    setTime(day, month, dow, hr, min, sec, ampm, colons)
+    setInterval(()=> setTime(day, month, dow, hr, min, sec, ampm, colons), 1000);
+});
+
+
+
+
+function setTime(day, month, dow, hr, min, sec, ampm, colons) {
+    // Date value
+    const now = new Date();
+    
+    day.innerText = now.getDate();
+    month.innerText = months[ now.getMonth() ];
+    dow.innerText = `(${dayOfWeek[ now.getDay() - 1 ]})`;
+    hr.innerText = `${(now.getHours() % 12 === 0)? 12: now.getHours() % 12}`.padStart(2, "0");
+    min.innerText = `${now.getMinutes()}`.padStart(2, "0");
+    sec.innerText = `${now.getSeconds()}`.padStart(2, "0");
+    ampm.innerText = (now.getHours() >= 12)? "PM": "AM";
+
+}
