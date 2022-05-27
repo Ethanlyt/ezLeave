adb086ca8f6726e07d22a81bf2a3ee7c4c63e0c6
Ethanlyt <ethanleong86@gmail.com>
2022-05-27 16:38:23 +0800

    v1.6

--- javascript/managerHome.js
+++ javascript/managerHome.js
@@ -16,21 +16,3 @@ function dropdownFunction() {
     }
   }
 
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdow.show
-        ns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
