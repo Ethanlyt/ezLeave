31a68e1caec5c37a78bdf09bd20f1ce144b93522
AdmiJW <jamweg22@gmail.com>
2022-05-28 04:09:14 +0800

    Progress - ManagerHome

--- javascript/managerHome.js
+++ javascript/managerHome.js
@@ -1,18 +1,18 @@
 function dropdownFunction() {
     document.getElementById("SortDropdown").classList.toggle("show");
-  }
-  
+}
 
-  window.onclick = function(event) {
+
+window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-  }
+}
 
